2d5de0615adc18e46a329ed9f2d600b636813aa4
Felipe Martins <me@fefas.net>
2016-04-26 18:07:49 -0300

    chapter 2

---
+++ test/iloveyouboss/ProfileTest.java
@@ -0,0 +1,40 @@
+package iloveyouboss;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ProfileTest {
+	private Profile profile;
+	private BooleanQuestion question;
+	private Criteria criteria;
+	
+	@Before
+	public void setUp()
+	{
+		profile = new Profile("Bull Hockey, Inc.");
+		question = new BooleanQuestion(1, "Got bonuses?");
+		criteria = new Criteria();
+	}
+	
+	@Test
+	public void matchAnswersFalseWhenMustMatchCriteriaNotMet()
+	{
+		profile.add(new Answer(question, Bool.FALSE));
+		criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+		
+		boolean matches = profile.matches(criteria);
+		
+		assertFalse(matches);
+	}
+
+	@Test
+	public void matchAnswersTrueForAnyDontCareCriteria()
+	{
+		profile.add(new Answer(question, Bool.FALSE));
+		criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+		
+		boolean matches = profile.matches(criteria);
+		
+		assertTrue(matches);
+	}
+}
